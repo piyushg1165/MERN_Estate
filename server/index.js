96317d61080f9f1b457d41373c94ce7a8b40ee25
Piyush Gupta <piyushgupta1165@gmail.com>
2025-07-06 13:27:12 +0530

    created sign up api route and added bcrypt

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
+import authRouter from './routes/authRoute.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -12,8 +13,12 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 }); 
 
 app.use("/api/user", userRouter);
+
+app.use("/api/auth", authRouter);
