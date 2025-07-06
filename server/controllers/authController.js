782943460bbbb59e10bf9bbd401fbef722fec733
Piyush Gupta <piyushgupta1165@gmail.com>
2025-07-06 16:09:27 +0530

    added signin  api route with jwt fuctionlity

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => { 
     const { username, email, password } = req.body;
@@ -15,6 +17,34 @@ export const signup = async (req, res, next) => {
             res.status(201).json({
                 message: "User created successfully",
             });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return next(errorHandler(404, "User not found"));
+        }
+
+        const isMatchPassword = bcrypt.compareSync(password, user.password);
+        if (!isMatchPassword) {
+            return next(errorHandler(401, "Wrong Credentials"));
+        }
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET
+        );
+        const { password: _, ...rest } = user._doc; 
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        })
+        .status(200)
+        .json(rest);
     } catch (error) {
         next(error);
     }
